f81a81b9679d0770c8ffab66cf353aa7b439c4a2
woosajang <100646302+woosajang@users.noreply.github.com>
2022-03-06 13:30:28 +0900

    Update bitcoinautotrade_others.py

--- bitcoinautotrade_others.py
+++ bitcoinautotrade_others.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "S7EQC8OdSCSqDeQz8xFozCOHw2DW7qPPKu1qp1vx"
-secret = "ozCbzFCxakxVCx8htKqgWI6FxLdnGsjKSIfluGmE"
+access = ""
+secret = ""
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
